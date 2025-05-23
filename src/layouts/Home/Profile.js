809200e7f8129c6a7cd0d7d1ff76006f0854865d
tomirodeghiero <tomyrodeghiero@gmail.com>
2025-05-23 15:14:12 +0000

    Update personal description

--- src/layouts/Home/Profile.js
+++ src/layouts/Home/Profile.js
@@ -22,12 +22,9 @@ const ProfileText = ({ visible, titleId }) => (
 
     {/* prettier-ignore */}
     <Text className={styles.description} data-visible={visible} size="l" as="p">
-      I currently work as a Software Developer at <Link href="https://futitservices.com">Futit Services</Link>, 
-      where I blend my frontend expertise with backend know-how to create seamless digital experiences.
-      Alongside my professional work, I&apos;m furthering my education at the National
-      University of Río Cuarto. I firmly believe that the cornerstone of extraordinary
-      projects and products lies in cohesive and dynamic teams collaborating,
-      experimenting, and learning together fuels my commitment to collective success.
+      I am a Computer Analyst, graduated from the National University of Río Cuarto, where I developed strong skills in analytical reasoning and programming. Currently, continuing my studies in the Bachelor’s Degree in Computer Science at the same university.
+      I’m passionate about creating new things and implementing solutions to unmet needs. However, I firmly believe that great projects, products, and creations stem from exceptional teamwork: sharing a common goal, having clear roles, experimenting, and learning together.
+      For this reason, I enjoy meeting and working with different people, always maintaining an attitude of learning from them to later share that knowledge.
     </Text>
     <Text className={styles.description} data-visible={visible} size="l" as="p">
       I&apos;m always keen to explore new opportunities and ideas, so feel free to reach
